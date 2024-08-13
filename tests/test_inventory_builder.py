6fc682895d6bda94918ecd3f5ce0c7e926a0e4e0
catherinegomes <cgomestx@gmail.com>
2024-08-13 08:25:57 -0400

    add test markers

--- tests/test_inventory_builder.py
+++ tests/test_inventory_builder.py
@@ -1,33 +1,43 @@
+import pytest
+
 from src import inventory_builder
 
 
+@pytest.mark.unittest
 def test_write_shelves():
     pass
 
 
+@pytest.mark.unittest
 def test_write_racks():
     pass
 
 
+@pytest.mark.unittest
 def test_write_drawers_or_rows():
     pass
 
 
+@pytest.mark.unittest
 def test_write_boxes():
     pass
 
 
+@pytest.mark.unittest
 def test_post_parent_location():
     pass
 
 
+@pytest.mark.unittest
 def test_extend_list():
     pass
 
 
+@pytest.mark.unittest
 def test_post_child_location():
     pass
 
 
+@pytest.mark.unittest
 def test_post_box():
     pass
